9ac9d5bf2f7a9435f774118f7da46e75da29a1ae
nikiforovaE <nikiforovaa.ekaterinaa@gmail.com>
2026-03-28 13:58:08 +0300

    feat: add mongo repositories

--- src/main/java/org/example/eventhub/Main.java
+++ src/main/java/org/example/eventhub/Main.java
@@ -4,9 +4,11 @@ import org.example.eventhub.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppConfig.class)
+@EnableMongoRepositories(basePackages = "org.example.eventhub.repository")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
