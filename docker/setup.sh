517c801b7535396c7b1eef71dd4f6229858a1da2
nikiforovaE <nikiforovaa.ekaterinaa@gmail.com>
2026-05-03 01:50:48 +0300

    build: cassandra setup

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 until cqlsh ${CASSANDRA_HOSTS} -e "DESCRIBE KEYSPACES"; do
   echo "Cassandra is unavailable - sleeping"
   sleep 5
 done
 
 export CLEAN_KS=$(echo "${CASSANDRA_KEYSPACE}" | tr -d '"')
-envsubst < /init.cql > /tmp/init_ready.cql
+
+sed "s/\${CASSANDRA_KEYSPACE}/${CLEAN_KS}/g" /init.cql > /tmp/init_ready.cql
+
 cqlsh ${CASSANDRA_HOSTS} -f /tmp/init_ready.cql
