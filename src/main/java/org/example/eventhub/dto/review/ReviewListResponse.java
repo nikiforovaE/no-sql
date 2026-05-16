9738444d053e1f64abe62f8550da734b88e9d1ce
nikiforovaE <nikiforovaa.ekaterinaa@gmail.com>
2026-05-16 04:04:07 +0300

    docs: add swagger annotations

--- src/main/java/org/example/eventhub/dto/review/ReviewListResponse.java
+++ src/main/java/org/example/eventhub/dto/review/ReviewListResponse.java
@@ -1,5 +1,6 @@
 package org.example.eventhub.dto.review;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,7 +8,11 @@ import java.util.List;
 
 @Data
 @Builder
+@Schema(description = "Ответ со списком отзывов и общим количеством отзывов")
 public class ReviewListResponse {
+    @Schema(description = "Список найденных отзывов")
     private List<ReviewResponse> reviews;
+
+    @Schema(description = "Общее количество найденных отзывов", example = "1")
     private int count;
 }
--- src/main/java/org/example/eventhub/dto/review/ReviewResponse.java
+++ src/main/java/org/example/eventhub/dto/review/ReviewResponse.java
@@ -1,5 +1,6 @@
 package org.example.eventhub.dto.review;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,14 +8,29 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@Schema(description = "Данные конкретного отзыва")
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReviewResponse {
+
+    @Schema(description = "ID отзыва", example = "56e2c0b3a2b4c1a5e6f7f8b3")
     private String id;
+
+    @Schema(description = "ID мероприятия в MongoDB", example = "12e9c0b1a2b3c3d5e6f7a8b7")
     private String event_id;
+
+    @Schema(description = "Комментарий пользователя", example = "Великолепный спектакль! Идите, даже не думайте!")
     private String comment;
-    private String created_at; // Формат RFC3339
-    private String created_by;
+
+    @Schema(description = "Оценка от 1 до 5", example = "5")
     private int rating;
+
+    @Schema(description = "Дата создания (RFC3339)", example = "2026-03-14T14:59:32+03:00")
+    private String created_at;
+
+    @Schema(description = "ID автора отзыва", example = "65e9c0b1a2b3c4d5e6f7a8b9")
+    private String created_by;
+
+    @Schema(description = "Дата обновления (RFC3339)", example = "2026-03-14T14:59:32+03:00")
     private String updated_at;
 }
