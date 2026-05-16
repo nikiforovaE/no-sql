88ea8e026f186b59443efa518f09611c4416539f
nikiforovaE <nikiforovaa.ekaterinaa@gmail.com>
2026-05-16 04:06:59 +0300

    docs: add swagger annotations

--- src/main/java/org/example/eventhub/dto/review/ReviewStatsResponse.java
+++ src/main/java/org/example/eventhub/dto/review/ReviewStatsResponse.java
@@ -1,5 +1,6 @@
 package org.example.eventhub.dto.review;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,18 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Усредненная информация об отзывах на мероприятие (данные из Redis)")
 public class ReviewStatsResponse {
+
+    @Schema(
+            description = "Общее количество всех отзывов на мероприятие (по его названию)",
+            example = "123"
+    )
     private int count;
+
+    @Schema(
+            description = "Средний рейтинг всех отзывов, округлённый до одного знака после запятой",
+            example = "4.8"
+    )
     private double rating;
 }
