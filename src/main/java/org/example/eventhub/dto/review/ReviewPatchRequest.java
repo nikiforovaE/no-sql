f5e6015d3125e5427a3634219ff18a72bb9a99f7
nikiforovaE <nikiforovaa.ekaterinaa@gmail.com>
2026-05-16 04:19:24 +0300

    docs: add swagger annotations

--- src/main/java/org/example/eventhub/dto/review/ReviewPatchRequest.java
+++ src/main/java/org/example/eventhub/dto/review/ReviewPatchRequest.java
@@ -1,9 +1,23 @@
 package org.example.eventhub.dto.review;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Запрос на частичное изменение отзыва")
 public class ReviewPatchRequest {
+
+    @Schema(
+            description = "Новая оценка от 1 до 5. Только если нужно изменить.",
+            example = "3",
+            minimum = "1",
+            maximum = "5"
+    )
     private Integer rating;
+
+    @Schema(
+            description = "Новый комментарий (макс. 300 символов). Только если нужно изменить.",
+            example = "На самом деле, так себе спектакль..."
+    )
     private String comment;
 }
